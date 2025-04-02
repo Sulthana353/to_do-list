2295518ec65cb96a1c34ae5c6aa189e17e361888
Sulthana353 <Sulthanathazneem@gmail.com>
2025-04-02 17:34:38 +0530

    third commit

--- script.js
+++ script.js
@@ -9,11 +9,28 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listcontainer.appendChild(li);
-        let span = document.getElementById("span");
+        let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
 }
 
-listcontainer.addEventListener("click",function())
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
